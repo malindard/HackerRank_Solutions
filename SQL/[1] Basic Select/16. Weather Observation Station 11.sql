a93cd5e69cfd96934f50492368b256ec543fd64c
malindard <91930104+malindard@users.noreply.github.com>
2023-01-09 20:49:09 +0700

    Create 16. Weather Observation Station 11.sql

---
+++ SQL/[1] Basic Select/16. Weather Observation Station 11.sql
@@ -0,0 +1,18 @@
+/*
+Link: https://www.hackerrank.com/challenges/weather-observation-station-11/problem?isFullScreen=true
+Difficulty: easy
+Max score: 15
+*/
+
+-- with MySQL
+SELECT DISTINCT city
+FROM station
+WHERE NOT (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%')
+OR 
+NOT (city LIKE '%A' OR city LIKE '%E' OR city LIKE '%I' OR city LIKE '%O' OR city LIKE '%U');
+
+-- or this (with MySQL)
+SELECT DISTINCT city FROM station 
+WHERE LEFT(city,1) NOT IN ('a','e','i','o','u') 
+OR 
+RIGHT(city,1) NOT IN ('a','e','i','o','u');
