aab1782bd37c5b187eaca30009a169bc567a92d7
malindard <91930104+malindard@users.noreply.github.com>
2023-01-09 13:46:58 +0700

    Create [5] Japanese Cities' Attributes.sql

---
+++ SQL/[1] Basic Select/[5] Japanese Cities' Attributes.sql
@@ -0,0 +1,7 @@
+/*
+Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+Difficulty: easy
+Max score: 10
+*/
+
+SELECT * FROM city WHERE countrycode = 'JPN';
