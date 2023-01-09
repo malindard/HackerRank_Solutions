107a5a3170136eaa8d7e63beab77c8fb5c533eac
malindard <91930104+malindard@users.noreply.github.com>
2023-01-09 21:47:51 +0700

    Create 04. Python: Division.py

---
+++ Python/01. Introduction/04. Python: Division.py
@@ -0,0 +1,10 @@
+'''
+Link: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+Difficulty: Easy
+Max score: 10
+'''
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
